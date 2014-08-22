862be932590532cd50f3619c21a5673cd95b5a30
KoryNunn <knunn187@gmail.com>
2014-08-22 11:18:33 +1000

    working hackily

--- index.built.js
+++ index.built.js
@@ -1,33 +1,205 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/home/kory/dev/toasthub/index.js":[function(require,module,exports){
 var Ajax = require('simple-ajax'),
     githubApi = 'https://api.github.com';
-    githubUrl = 'https://github.com';
+    githubUrl = 'https://github.com',
+    crel = require('crel'),
+    seen = [];
 
 function parseHtml(html){
     var el = document.createElement('div');
 
     el.innerHTML = html;
 
-    return el;
+    var notifications = el.querySelectorAll('#notification-center .notifications-list .list-group-item');
+
+    var results = [],
+        newSeen = [];
+
+    for (var i = 0; i < notifications.length; i++) {
+        var notification = notifications[i],
+            link = notification.querySelector('.js-notification-target'),
+            result = {};
+
+        result.text = link.textContent;
+        result.link = link.getAttribute('href');
+        result.id = notification.getAttribute('data-note-id');
+        result.image = notification.querySelector('img.avatar').getAttribute('src');
+
+        newSeen.push(result.id);
+
+        if(~seen.indexOf(result.id)){
+            continue;
+        }
+
+        results.push(result);
+    };
+
+    seen = newSeen;
+
+    return results;
+}
+
+function showDesktopNotification(results){
+    results.forEach(function(result){
+        var notification = new Notification('Toasthub', {
+            body: result.text,
+            icon: result.image
+        });
+
+        notification.onclick = function(){
+            window.open(result.link);
+        };
+    })
 }
 
 function checkNotifications(){
     var request = new Ajax(githubUrl + '/notifications');
 
     request.on('complete', function(response){
-        var x = parseHtml(response.target.responseText);
-        console.log(response);
+        var statusCode = response.target.status;
+        if(statusCode === 200){
+            showDesktopNotification(parseHtml(response.target.responseText));
+        }
+        setTimeout(checkNotifications, 60000);
     });
 
     request.send();
 }
 
-var notification = new Notification(
-  'Thing!'
-);
-
 checkNotifications();
-},{"simple-ajax":"/home/kory/dev/toasthub/node_modules/simple-ajax/index.js"}],"/home/kory/dev/toasthub/node_modules/simple-ajax/index.js":[function(require,module,exports){
+},{"crel":"/home/kory/dev/toasthub/node_modules/crel/crel.js","simple-ajax":"/home/kory/dev/toasthub/node_modules/simple-ajax/index.js"}],"/home/kory/dev/toasthub/node_modules/crel/crel.js":[function(require,module,exports){
+//Copyright (C) 2012 Kory Nunn
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/*
+
+    This code is not formatted for readability, but rather run-speed and to assist compilers.
+
+    However, the code's intention should be transparent.
+
+    *** IE SUPPORT ***
+
+    If you require this library to work in IE7, add the following after declaring crel.
+
+    var testDiv = document.createElement('div'),
+        testLabel = document.createElement('label');
+
+    testDiv.setAttribute('class', 'a');
+    testDiv['className'] !== 'a' ? crel.attrMap['class'] = 'className':undefined;
+    testDiv.setAttribute('name','a');
+    testDiv['name'] !== 'a' ? crel.attrMap['name'] = function(element, value){
+        element.id = value;
+    }:undefined;
+
+
+    testLabel.setAttribute('for', 'a');
+    testLabel['htmlFor'] !== 'a' ? crel.attrMap['for'] = 'htmlFor':undefined;
+
+
+
+*/
+
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else {
+        root.crel = factory();
+    }
+}(this, function () {
+    // based on http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+    var isNode = typeof Node === 'function'
+        ? function (object) { return object instanceof Node; }
+        : function (object) {
+            return object
+                && typeof object === 'object'
+                && typeof object.nodeType === 'number'
+                && typeof object.nodeName === 'string';
+        };
+    var isArray = function(a){ return a instanceof Array; };
+    var appendChild = function(element, child) {
+      if(!isNode(child)){
+          child = document.createTextNode(child);
+      }
+      element.appendChild(child);
+    };
+
+
+    function crel(){
+        var document = window.document,
+            args = arguments, //Note: assigned to a variable to assist compilers. Saves about 40 bytes in closure compiler. Has negligable effect on performance.
+            element = args[0],
+            child,
+            settings = args[1],
+            childIndex = 2,
+            argumentsLength = args.length,
+            attributeMap = crel.attrMap;
+
+        element = isNode(element) ? element : document.createElement(element);
+        // shortcut
+        if(argumentsLength === 1){
+            return element;
+        }
+
+        if(typeof settings !== 'object' || isNode(settings) || isArray(settings)) {
+            --childIndex;
+            settings = null;
+        }
+
+        // shortcut if there is only one child that is a string
+        if((argumentsLength - childIndex) === 1 && typeof args[childIndex] === 'string' && element.textContent !== undefined){
+            element.textContent = args[childIndex];
+        }else{
+            for(; childIndex < argumentsLength; ++childIndex){
+                child = args[childIndex];
+
+                if(child == null){
+                    continue;
+                }
+
+                if (isArray(child)) {
+                  for (var i=0; i < child.length; ++i) {
+                    appendChild(element, child[i]);
+                  }
+                } else {
+                  appendChild(element, child);
+                }
+            }
+        }
+
+        for(var key in settings){
+            if(!attributeMap[key]){
+                element.setAttribute(key, settings[key]);
+            }else{
+                var attr = crel.attrMap[key];
+                if(typeof attr === 'function'){
+                    attr(element, settings[key]);
+                }else{
+                    element.setAttribute(attr, settings[key]);
+                }
+            }
+        }
+
+        return element;
+    }
+
+    // Used for mapping one kind of attribute to the supported version of that in bad browsers.
+    // String referenced so that compilers maintain the property name.
+    crel['attrMap'] = {};
+
+    // String referenced so that compilers maintain the property name.
+    crel["isNode"] = isNode;
+
+    return crel;
+}));
+
+},{}],"/home/kory/dev/toasthub/node_modules/simple-ajax/index.js":[function(require,module,exports){
 var EventEmitter = require('events').EventEmitter;
 
 function tryParseJson(data){
--- index.js
+++ index.js
@@ -1,28 +1,68 @@
 var Ajax = require('simple-ajax'),
     githubApi = 'https://api.github.com';
-    githubUrl = 'https://github.com';
+    githubUrl = 'https://github.com',
+    crel = require('crel'),
+    seen = [];
 
 function parseHtml(html){
     var el = document.createElement('div');
 
     el.innerHTML = html;
 
-    return el;
+    var notifications = el.querySelectorAll('#notification-center .notifications-list .list-group-item');
+
+    var results = [],
+        newSeen = [];
+
+    for (var i = 0; i < notifications.length; i++) {
+        var notification = notifications[i],
+            link = notification.querySelector('.js-notification-target'),
+            result = {};
+
+        result.text = link.textContent;
+        result.link = link.getAttribute('href');
+        result.id = notification.getAttribute('data-note-id');
+        result.image = notification.querySelector('img.avatar').getAttribute('src');
+
+        newSeen.push(result.id);
+
+        if(~seen.indexOf(result.id)){
+            continue;
+        }
+
+        results.push(result);
+    };
+
+    seen = newSeen;
+
+    return results;
+}
+
+function showDesktopNotification(results){
+    results.forEach(function(result){
+        var notification = new Notification('Toasthub', {
+            body: result.text,
+            icon: result.image
+        });
+
+        notification.onclick = function(){
+            window.open(result.link);
+        };
+    })
 }
 
 function checkNotifications(){
     var request = new Ajax(githubUrl + '/notifications');
 
     request.on('complete', function(response){
-        var x = parseHtml(response.target.responseText);
-        console.log(response);
+        var statusCode = response.target.status;
+        if(statusCode === 200){
+            showDesktopNotification(parseHtml(response.target.responseText));
+        }
+        setTimeout(checkNotifications, 60000);
     });
 
     request.send();
 }
-
-var notification = new Notification(
-  'Thing!'
-);
 
 checkNotifications();
